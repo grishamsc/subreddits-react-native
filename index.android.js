3363ef0c74d0b56bebdd42441ea512f691f48db7
Grigory <gs@messapps.com>
2017-04-15 09:42:10 +0300

    Added android

--- index.android.js
+++ index.android.js
@@ -1,53 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator
 } from 'react-native';
 
+import Posts from "./app/components/Posts/Posts"
+import HeaderBar from "./app/components/HeaderBar/HeaderBar"
+import PostDetails from "./app/components/PostDetails/PostDetails"
+
 export default class subreddits extends Component {
+
+  renderScene(route, navigator) {
+    switch(route.id) {
+      case "posts":
+        return (
+          <View style = {styles.container}>
+            <HeaderBar />
+            <Posts navigator={navigator} title = "posts" />
+          </View>
+        )
+      case "details":
+        return (
+           <View style = {styles.container}>
+            <HeaderBar />
+            <PostDetails navigator={navigator} post = {route.post} title = "details" />
+          </View>
+        )
+    } 
+  }
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{ id: "posts" }}
+        renderScene={this.renderScene}
+        configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromRight}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 20
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 AppRegistry.registerComponent('subreddits', () => subreddits);
